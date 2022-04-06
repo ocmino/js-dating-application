502b5f108beb57e7f4efc7ac265408345d313c91
Jonathan Lindqvist <ocmino@gmail.com>
2022-04-06 14:28:06 +0200

    remove test code

--- JS/numberInput.js
+++ JS/numberInput.js
@@ -1,8 +1,7 @@
-
-   function changeTheColorOfButtonDemo() {
-      if (document.getElementById("changeColorDemo").value !== "") {
-         document.getElementById("buttonDemo").style.background = "#9234E4";
-      } else {
-         document.getElementById("buttonDemo").style.background = "#D0D5DD";
-      }
+function changeTheColorOfButtonDemo() {
+   if (document.getElementById("changeColorDemo").value !== "") {
+      document.getElementById("buttonDemo").style.background = "#9234E4";
+   } else {
+      document.getElementById("buttonDemo").style.background = "#D0D5DD";
    }
+}
