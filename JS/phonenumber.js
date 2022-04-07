a9aa4fe025d50a77ea178775c938e47d2cf6172e
Jonathan Lindqvist <ocmino@gmail.com>
2022-04-07 12:40:46 +0200

    fix color of button, change text color on input, change input type to number

--- JS/phonenumber.js
+++ JS/phonenumber.js
@@ -1,9 +1,14 @@
 function changeTheColorOfButtonDemo() {
-    console.log(document.getElementsByClassName("changeColorOfButton")[0].value);
+
     if (document.getElementsByClassName("changeColorOfButton")[0].value !== "") {
        document.getElementById("buttonDemo").style.background = "#9234E4";
+       document.getElementById("buttonDemo").style.color = "#FFFFFF";
+       
+   
     } else {
        document.getElementById("buttonDemo").style.background = "#D0D5DD";
+       document.getElementById("buttonDemo").style.color = "#64748B";
+
     }
     
  }
